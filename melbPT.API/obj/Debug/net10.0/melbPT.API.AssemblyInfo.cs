91314dd615c85c96bf6db9c23487a3b249c0327e
Agam Singh <agamv2525@gmail.com>
2026-04-21 05:15:35 +1000

    API Keys

--- melbPT.API/obj/Debug/net10.0/melbPT.API.AssemblyInfo.cs
+++ melbPT.API/obj/Debug/net10.0/melbPT.API.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("ee2ba18e-591e-4839-a6fe-59e200aafca4")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("melbPT.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3133b5bd235c2f2c538762c4566291ab571428da")]
 [assembly: System.Reflection.AssemblyProductAttribute("melbPT.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("melbPT.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
